b904b1ed634407efd0ded5b98fc2a300a51b947d
Joseph Watkins <napkinsterror@gmail.com>
2016-11-18 21:48:24 -0500

    add xlsx extension

--- automation/event_csv_script.py
+++ automation/event_csv_script.py
@@ -1090,7 +1090,7 @@ def perform_data_converstion(request):
     file_location = excelBuilder.save("/tmp", event["event_name"])
     print_files_in_folder("/tmp")
 
-    response = upload_file_to_dropbox(file_location, event["event_name"])
+    response = upload_file_to_dropbox(file_location, event["event_name"] + ".xlsx")
     print("Completed: " + str(file_location))
     print("Response: " + str(response))
 
