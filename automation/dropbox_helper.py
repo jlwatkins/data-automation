7bb78c82a8eae59a0640ac7cddd43d7cc93b9bba
Joseph Watkins <napkinsterror@gmail.com>
2016-11-18 21:39:15 -0500

    no message

--- automation/dropbox_helper.py
+++ automation/dropbox_helper.py
@@ -7,8 +7,9 @@ access_token = 'kSVIwQWSbHMAAAAAAAAL64PmaxHp3J-LHwFp-f0XC9J2nx5Ef_MCNHYGbFAeG2LA
 
 def upload_file_to_dropbox(file_location, filename):
     metadata = None
-    with open(file_location, 'rb') as f:
-        dbx = dropbox.Dropbox(access_token)
-        dbx.users_get_current_account()
-        metadata = dbx.files_upload(f, "/data/" + str(filename))
+    f = open('working-draft.txt', 'rb')
+    dbx = dropbox.Dropbox(access_token)
+    dbx.users_get_current_account()
+    metadata = dbx.files_upload(f, str(filename))
+
     return metadata
