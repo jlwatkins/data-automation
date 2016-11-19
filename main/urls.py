f950b85d901cbcad81210af95041bf0ae5da15fc
Joseph Watkins <napkinsterror@gmail.com>
2016-11-18 19:44:33 -0500

    wrong url call

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^data/', automation.event_csv_script.get_column_letter)
+    url(r'^data/', automation.event_csv_script.perform_data_converstion)
 ]
